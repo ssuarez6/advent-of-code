bd14ef17119c007bc8b03d58ed472f43cfa3ed3d
Santiago Suarez <suarezsantiagoperez@gmail.com>
2024-12-13 17:43:14 -0500

    12

---
+++ 2024/12/partOne.scala
@@ -0,0 +1,105 @@
+import scala.io.Source
+import scala.collection.mutable.Queue
+import scala.collection.mutable.HashSet
+import scala.collection.{mutable => mut}
+
+object GardenGroups {
+
+  type Region = Vector[(Int, Int)]
+
+
+  def cardinalPositions(x: Int, y: Int): List[(Int, Int)] = 
+    List((x-1, y), (x+1, y), (x, y-1), (x, y+1))
+
+  def neighborPositions(ix: Int, iy: Int): List[(Int, Int)] = {
+    (ix -1 to ix + 1).flatMap{x =>
+      (iy -1 to iy + 1).flatMap {y => 
+        Option.when(x != ix || y != iy)((x, y))
+      }
+    }.toList
+  }
+
+  case class PlantMap(plants: Vector[String]) {
+    val height: Int = plants.size
+    val width: Int = plants.head.size
+
+    def apply(x: Int, y: Int): Char = plants(y)(x)
+
+    def isDefinedAt(x: Int, y: Int): Boolean = 
+      x >= 0 && x < width && y >= 0 && y < height
+
+    def get(x: Int, y: Int): Option[Char] = 
+      Option.when(isDefinedAt(x, y))(apply(x, y))
+
+    def floodFill(x: Int, y: Int): Region = {
+      val q = Queue[(Int, Int)]()
+      val char = apply(x, y)
+      val res = mut.ListBuffer[(Int, Int)]()
+      q.addOne((x, y))
+      while (q.nonEmpty) {
+        val n  = q.removeHead()
+        if(get(n._1, n._2).contains(char) && !res.contains(n)) {
+          res.prepend(n)
+          q.addAll(cardinalPositions(n._1, n._2))
+        }
+      }
+      res.toVector
+    }
+
+    def indices: Vector[(Int, Int)] = 
+      (for {
+        y <- 0 until height
+        x <- 0 until width
+      } yield (x, y)).toVector
+
+    def regions: List[Region] = {
+      List.unfold[Vector[(Int, Int)], Vector[(Int, Int)]](this.indices) { acc =>
+        acc.headOption.map {head =>
+          val points = floodFill(head._1, head._2)
+          (points, acc.diff(points))
+        }
+      }
+    }
+
+    def optionalNeighbors(x: Int, y: Int): List[Option[Char]] = 
+      neighborPositions(x, y).map(pair => get(pair._1, pair._2))
+
+    def optionalCardinalNeighbors(x: Int, y: Int): List[Option[Char]] = 
+      cardinalPositions(x, y).map{case (xx, yy) => get(xx, yy)}
+    
+  }
+
+
+  implicit class RegionSyntax(region: Region) {
+    def asPlantMap: PlantMap = {
+      val maxX = region.maxBy(_._1)._1
+      val maxY = region.maxBy(_._2)._2
+      val res = mut.ArrayBuffer.fill(maxY + 1, maxX + 1)('.')
+      region.foreach { case (x, y) => 
+        res(y)(x) = '#'
+      }
+      PlantMap(res.map(_.mkString("", "", "")).toVector)
+    }
+
+    def inflate: Region =
+      region.flatMap{
+        case (x, y) => List((x * 2, y * 2), (x * 2 + 1, y * 2), (x * 2, y * 2 + 1), (x * 2 + 1, y * 2 + 1))
+      }
+    
+
+    def perimeter: Int = {
+      val regionMap = region.asPlantMap
+      region.map{case (x, y) => regionMap.optionalCardinalNeighbors(x, y).count(_.forall(_ != '#'))}.sum
+    }
+
+    def area: Int = region.size
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    val input = Source.fromFile("input.txt").getLines.toVector
+    val plants = PlantMap(input)
+    val solution = plants.regions.map(r => r.area * r.perimeter).sum
+    println(s"Solution: $solution")
+  }
+}
